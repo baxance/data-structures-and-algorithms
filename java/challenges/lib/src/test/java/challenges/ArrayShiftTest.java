3297d12abac4bf2e41da7129aa492c8488b1fd9d
Barrett <baxance@protonmail.com>
2021-03-23 18:20:50 -0700

    additional unit tests

--- java/challenges/lib/src/test/java/challenges/ArrayShiftTest.java
+++ java/challenges/lib/src/test/java/challenges/ArrayShiftTest.java
@@ -12,4 +12,20 @@ public class ArrayShiftTest {
     int[] expected = {1, 2, 5, 3, 4};
     assertArrayEquals(expected, actual);
   }
+  @Test
+  public void insertShiftArrayTest2() {
+    int[] arr = {11, 222, 345, 4659, 698301, 7802759};
+    int num = 50592;
+    int[] actual = ArrayShift.insertShiftArray(arr, num);
+    int[] expected = {11, 222, 345, 50592, 4659, 698301, 7802759};
+    assertArrayEquals(expected, actual);
+  }
+  @Test
+  public void insertShiftArrayTest3() {
+    int[] arr = {1};
+    int num = 2;
+    int[] actual = ArrayShift.insertShiftArray(arr, num);
+    int[] expected = {1, 2};
+    assertArrayEquals(expected, actual);
+  }
 }
