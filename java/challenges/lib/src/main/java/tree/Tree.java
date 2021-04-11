256c344fa5bcba58f2aa1f49cd0f0ce101cf6d6b
Barrett <baxance@protonmail.com>
2021-04-11 13:04:05 -0700

    cc15

---
+++ java/challenges/lib/src/main/java/tree/Tree.java
@@ -0,0 +1,17 @@
+package tree;
+
+public class Tree {
+  public Node root;
+
+  public Tree(){
+    this.root = null;
+  }
+
+  public Object[] preOrder(Node root){
+    if (root != null){
+      (root.left);
+    }
+
+  }
+
+}
